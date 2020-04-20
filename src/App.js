5bee0dffb6ec74e609d5cf8c120cb3d5d8949be1
SachinSh97 <sachink1.kvctps@gmail.com>
2020-04-20 23:02:50 +0530

    component for input and message list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Messages from "./components/Messages";
+import "./App.css";
+import Input from "./components/Input";
+import { randomName, randomColor } from "./userInfo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    messages: [
+      {
+        text: "This is a test message!",
+        member: {
+          color: "blue",
+          username: "bluemoon",
+        },
+      },
+    ],
+    member: {
+      username: randomName(),
+      color: randomColor(),
+    },
+  };
+  onSendMessage = (message) => {
+    const messages = this.state.messages;
+    messages.push({
+      text: message,
+      member: this.state.member,
+    });
+    this.setState({ messages: messages });
+  };
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>Mini Messenger</h1>
+        </div>
+        <Messages
+          messages={this.state.messages}
+          currentMember={this.state.member}
+        />
+        <Input onSendMessage={this.onSendMessage} />
+      </div>
+    );
+  }
 }
-
-export default App;
